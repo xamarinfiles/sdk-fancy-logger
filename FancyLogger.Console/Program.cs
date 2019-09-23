5d380d0d8c5df0d1581c076bfa168929b221bb06
Andy Lech <github@andylech.com>
2019-09-22 20:34:42 -0400

    Setup project structure
    - Add .Net Standard 2 library project
    - Add .Net Core 2.2 console application to test the library
    - Add VS Code debugging setup

---
+++ FancyLogger.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using FancyLogger;
+using System.Diagnostics;
+
+namespace FancyLogger.Console
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            FancyLogger = new FancyLoggerService();
+
+            System.Console.WriteLine("Hello World!");
+            Debug.WriteLine("Hello World!");
+        }
+
+        #region Properties
+
+        private static FancyLoggerService FancyLogger { get; set; }
+
+        #endregion
+    }
+}
