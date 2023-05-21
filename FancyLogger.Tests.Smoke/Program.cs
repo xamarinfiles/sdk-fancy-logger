dcebfbea00d446d27319f5a620d8f4790666df92
Andy Lech <github@andylech.com>
2023-05-21 19:20:37 -0400

    Recreate old Fancy Logger as iLogger extensions with assembly logger too

--- FancyLogger.Tests.Smoke/Program.cs
+++ FancyLogger.Tests.Smoke/Program.cs
@@ -1,10 +1,106 @@
-﻿namespace FancyLogger.Tests.Smoke
+﻿using System.Diagnostics;
+using System;
+using XamarinFiles.FancyLogger.Extensions;
+using static System.Net.HttpStatusCode;
+using static XamarinFiles.PdHelpers.Refit.Bundlers;
+
+namespace XamarinFiles.FancyLogger.Tests.Smoke
 {
-    internal class Program
+    internal static class Program
     {
+        #region Fields
+
+        private const string LoginFailedTitle = "Invalid Credentials";
+
+        private static readonly string[] LoginFailedUserMessages =
+        {
+            "Please check your Username and Password and try again"
+        };
+
+        #endregion
+
+        #region Services
+
+        private static FancyLoggerService? LoggerService { get; }
+
+        private static AssemblyLoggerService? AssemblyLoggerService { get; }
+
+        #endregion
+
+        #region Constructor
+
+        static Program()
+        {
+            try
+            {
+
+                LoggerService = new FancyLoggerService();
+                AssemblyLoggerService = new AssemblyLoggerService(LoggerService);
+            }
+            catch (Exception exception)
+            {
+                if (LoggerService is not null)
+                {
+                    LoggerService.LogExceptionRouter(exception);
+                }
+                else
+                {
+                    Debug.WriteLine("ERROR: Problem setting up logging services");
+                    Debug.WriteLine(exception);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Test Runner
+
         internal static void Main()
         {
+            try
+            {
+                /*  WARNING - Requires shared project copied from other repo  */
 
+                AssemblyLoggerService?.LogAssemblies(showCultureInfo: false);
+
+                /* /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\ */
+
+                if (LoggerService is null)
+                {
+                    return;
+                }
+
+                LoggerService?.LogHeader("FancyLogger Tests");
+
+                // TODO Add updated test set from old Fancy Logger
+
+                TestProblemDetailsLogger();
+            }
+            catch (Exception exception)
+            {
+                LoggerService?.LogExceptionRouter(exception);
+            }
         }
+
+        #endregion
+
+        #region Tests
+
+        private static void TestProblemDetailsLogger()
+        {
+            // 400 - BadRequest
+
+            var badRequestProblem =
+                BundleRefitProblemDetails(BadRequest,
+                    title: LoginFailedTitle,
+                    detail : "Invalid fields: Username, Password",
+                    userMessages: LoginFailedUserMessages);
+
+            LoggerService!.LogProblemDetails(badRequestProblem);
+
+            // TODO Add other ProblemDetails tests from other repo
+        }
+
+        #endregion
     }
 }
---
+++ FancyLogger/Serializer.cs
@@ -0,0 +1,124 @@
+﻿using Refit;
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using static XamarinFiles.PdHelpers.Refit.Extractors;
+
+namespace XamarinFiles.FancyLogger
+{
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+    internal class Serializer
+    {
+        #region Fields
+
+        #endregion
+
+        #region Constructor
+
+        internal Serializer(JsonSerializerOptions readJsonOptions,
+            JsonSerializerOptions writeJsonOptions)
+        {
+            ReadJsonOptions = readJsonOptions;
+            WriteJsonOptions = writeJsonOptions;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public JsonSerializerOptions ReadJsonOptions { get; }
+
+        public JsonSerializerOptions WriteJsonOptions { get; }
+
+        #endregion
+
+        #region Methods
+
+        public (string, ProblemDetails)
+            ToJson<T>(object obj, bool keepNulls = false)
+        {
+            if (obj is null)
+                // TODO Case where need to return message about null object?
+                return ("", null);
+
+            T typedObject;
+
+            // TODO Route when only simple object
+
+            if (obj is string str)
+            {
+                try
+                {
+                    if (typeof(T) == typeof(string))
+                    {
+                        // TODO Test if passed in obj is JSON
+                        return (str, null);
+                    }
+
+                    var deserializedObject =
+                        JsonSerializer.Deserialize<T>(str, ReadJsonOptions);
+
+                    typedObject = deserializedObject != null
+                        ? deserializedObject
+                        // Could also be NotSupportedException if JsonConverter
+                        : throw new JsonException();
+                }
+                catch (Exception exception)
+                {
+                    // TODO Only show beginning of long string in debug message
+                    var debugMessage =
+                        $"Unable to deserialize object of type {nameof(T)}: \"{str}\"";
+                    var problemDetails =
+                        ExtractRefitProblemDetails(exception,
+                            developerMessages: new[] { debugMessage });
+
+                    return (null, problemDetails);
+                }
+            }
+            else
+            {
+                try
+                {
+                    typedObject = (T)obj;
+                }
+                catch (Exception exception)
+                {
+                    // TODO Only show beginning of large object in debug message
+                    var debugMessage =
+                        $"Unable to cast object to type {nameof(T)}: \"{obj}\"";
+
+                    var problemDetails =
+                        ExtractRefitProblemDetails(exception,
+                            developerMessages: new[] { debugMessage });
+
+                    return (null, problemDetails);
+                }
+            }
+
+            try
+            {
+                var formattedJson =
+                    JsonSerializer.Serialize(typedObject, WriteJsonOptions);
+
+                return (formattedJson, null);
+            }
+            catch (Exception exception)
+            {
+                // TODO Only show beginning of large object in debug message
+                var debugMessage =
+                    $"Unable to serialize object of type {nameof(T)}: \"{typedObject}\"";
+
+                var problemDetails =
+                    ExtractRefitProblemDetails(exception,
+                        developerMessages: new[] { debugMessage });
+
+                return (null, problemDetails);
+            }
+        }
+
+        #endregion
+    }
+}
